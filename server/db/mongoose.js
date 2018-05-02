943c4a86d5b0a5973b6f920fd4b585c1ce55f092
baraon <gallegosbaraon@yahoo.com>
2018-05-02 11:05:18 -0500

    set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // tell mongoose to use ES6 promises
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds231749.mlab.com:31749/node-todo-test' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://baraon:node-test@ds231749.mlab.com:31749/node-todo-test' || 'mongodb://localhost:27017/TodoApp');
 
 module.export = {
   mongoose: mongoose
