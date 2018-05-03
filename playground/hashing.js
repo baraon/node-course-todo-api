f2b42bb02eb285dc25406e1d76e025a60f6f90ad
baraon <gallegosbaraon@yahoo.com>
2018-05-03 16:37:18 -0500

    added authentication, login, and logout

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,37 +1,17 @@
-//const {SHA256} = require('crypto-js');
+const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+/*bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});*/
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
-/*
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
+var hashedPassword = '$2a$10$G9fE6GVyrNSVJECNRX/6VODA66/hLMV79Wz3ODtgHcbxoRZMONtni';
 
-console.log('hash:', hash);
-console.log('message:', message)
-
-var data = {
-  id: 4
-};
-var token = {
-  data,
-  hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-};
-
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
-
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-if (resultHash === token.hash) {
-  console.log('Data was not changed');
-} else {
-  console.log('Data was changed. Do not trust!');
-}
-*/
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,21 +4,11 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
+const {User} = require('./../models/user');
 
-
-const todos = [{
-  _id : new ObjectID(),
-  text: 'first text to do'
-}, {
-  _id : new ObjectID(),
-  text: 'second text to do'
-}];
-
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
@@ -193,5 +183,119 @@ describe('GET /todos', () => {
           .expect(404)
           .end(done);
       });
+  });
+
+  describe('GET /users/me', () => {
+    it('should return user if authenticated', (done) => {
+      request(app)
+        .get('/users/me')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body._id).toBe(users[0]._id.toHexString());
+          expect(res.body.email).toBe(users[0].email);
+        })
+        .end(done);
+    });
+
+    it('should return 401 if not authenticated', (done) => {
+      request(app)
+        .get('/users/me')
+        .set('x-auth', '123')
+        .expect((res) => {
+          expect(res.body).toEqual({});
+        })
+        .expect(401)
+        .end(done);
+    });
+  });
+
+  describe('POST /users', () => {
+    it('should create a user', (done) => {
+      var email = 'example@example.com';
+      var password = '123abc';
+
+      request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(200)
+        .expect((res) => {
+          expect(res.headers['x-auth']).toExist();
+          expect(res.body._id).toExist();
+          expect(res.body.email).toBe(email);
+        })
+        .end((err) => {
+          if(err) {
+            return done(err);
+          }
+
+          User.findOne({email}).then((user) => {
+            expect(user).toExist();
+            expect(user.password).toNotBe(password);
+            done();
+          });
+        });
+    });
+
+    it('should return validation errors if request invalid', (done) => {
+      var email = 'not a valid email cuh';
+      var password = '';
+
+      request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(400)
+        .end(done);
+    });
+
+    it('should not create user if email in use', (done) => {
+      var email = 'test@test.com';
+      var password = '123';
+
+      request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(400)
+        .end(done);
+    });
+  });
+
+  describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+      request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+    });
+
+    it('should reject invalid login', (done) => {
+      request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'invalid password'
+      })
+      .expect(400)
+      .end(done);
+    });
   });
 });
