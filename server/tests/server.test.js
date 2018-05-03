63c70ce583cc57b9442a723895ba675d6c0e4184
baraon <gallegosbaraon@yahoo.com>
2018-05-03 16:48:27 -0500

    added delete and delete tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -298,4 +298,23 @@ describe('GET /todos', () => {
       .end(done);
     });
   });
+
+  describe('DELETE /users/me/token', () => {
+    it('should logout user and delete auth token', (done) => {
+      request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) => {
+          if(err) {
+            return done(err);
+          }
+
+          User.findById(users[0]._id).then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+        }).catch((e) => done(e));
+    });
+  });
+  });
 });
